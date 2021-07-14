6892a4b44cab54d897981f743cb695656bf26a3d
kiransripada <kiransripada@users.noreply.github.com>
2021-07-13 22:29:52 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 
+const outgoing_URL= 'https://swcompany.webhook.office.com/webhookb2/39bdc88e-8fff-4ba1-b60a-06049a6be189@44b79a67-d972-49ba-9167-8eb05f754a1a/IncomingWebhook/5e8f605f8afe4ffdb34cc26d11b9a3eb/6d139f40-a8a7-4485-a5ed-9de798a1a121';
+
 // ignore request for FavIcon. so there is no error in browser
 const ignoreFavicon = (req, res, next) => {
     if (req.originalUrl.includes('favicon.ico')) {
@@ -53,8 +55,8 @@ const create = async () => {
 
         const eventData =  eventGridEvent.data;
 
-        console.log('eventGridEvent ' + eventGridEvent);
-        console.log('eventData ' + eventData);
+        console.log('eventGridEvent ' + JSON.stringify(eventGridEvent));
+        console.log('eventData ' + JSON.stringify(eventData));
         console.log('******eventGridEvent*******');
 
 
@@ -70,6 +72,53 @@ const create = async () => {
 
         } else if (eventGridEvent.eventType == SecretNearExpiryEvent) {
             console.log('Got Blobcreated event data, blob URI ' + eventData.url);
+            const body = {
+                "@type": "MessageCard",
+                "@context": "http://schema.org/extensions",
+                "summary": "TESTING Azure Key Vault - Storage Account Secret Near Expiry Event",
+                "sections": [
+                    {
+                        "activityTitle": "TESTING Azure Key Vault - Storage Account Secret Near Expiry Event",
+                        "facts": [
+                            {
+                                "name": "Key Vault",
+                                "value": eventData.VaultName,
+                            },
+                            {
+                                "name": "Secret Name",
+                                "value": eventData.ObjectName,
+                            },
+                            {
+                                "name": "Secret Version",
+                                "value": eventData.Version,
+                            },
+                            {
+                                "name": "Secret Access URL",
+                                "value": eventData.Id,
+                            }
+                        ]
+                    }
+                ],
+                "potentialAction": [
+                    {
+                        "@context": "http://schema.org",
+                        "@type": "ViewAction",
+                        "name": "Learn more on Wikipedia",
+                        "target": [
+                            "https://en.wikipedia.org/wiki/Pluto"
+                        ]
+                    }
+                ]
+            }
+
+            const pos = await  fetch(outgoing_URL, {
+                method: 'post',
+                body: JSON.stringify(body),
+                headers: {'Content-Type': 'application/json'},
+            });
+            res.status(202).send("success!")
+
+
         } else if (eventGridEvent.eventType == CustomTopicEvent) {
             console.log('Got ContosoItemReceived event data, item SKU ' + eventData.itemSku);
         }else {
